49fa935ab24c9ef1f51981d2642cd113e8cff8c3
vccie <152461394+vccie@users.noreply.github.com>
2024-03-15 11:05:44 +0200

    Update _tokenspammer.py

--- util/functions/_tokenspammer.py
+++ util/functions/_tokenspammer.py
@@ -16,18 +16,19 @@ class spammer:
             tokens = f.readlines()
             for line in tokens:
                 token_list.append(line.replace("\n", ""))
-        token = random.choice(token_list)
-        url = f"https://discord.com/api/v9/channels/{id}/messages"
-        headers = {
-            "authorization": f"{token}"
-        }
-        payload = {
-            "content": f"".join(random.choice("1234567890")for _ in range(3))+ f" {msg}"
-        }
-        r = requests.post(url, headers=headers, json=payload, )
-        if r.status_code != 200:
-            cprint(token, "light_red")
-            spammer.token_uploader(id, msg)
-        else:
-            cprint(token, "light_green")
-            spammer.token_uploader(id, msg)
+        while True:
+            for token in token_list:
+                url = f"https://discord.com/api/v9/channels/{id}/messages"
+                headers = {
+                    "authorization": f"{token}"
+                }
+                payload = {
+                    "content": f"".join(random.choice("1234567890")for _ in range(3))+ f" {msg}"
+                }
+                r = requests.post(url, headers=headers, json=payload, )
+                if r.status_code != 200:
+                    cprint(token, "light_red")
+                    spammer.token_uploader(id, msg)
+                else:
+                    cprint(token, "light_green")
+                    spammer.token_uploader(id, msg)
